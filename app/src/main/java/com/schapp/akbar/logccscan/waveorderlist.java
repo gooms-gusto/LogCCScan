8b3013d5681ae63ca4c5ae6dc406af0d440ac808
Akbar <muhammad.a-akbar@dbschenker.com>
2017-12-21 16:46:56 +0700

    191217_5:49pm
    Signed-off-by: Akbar <muhammad.a-akbar@dbschenker.com>

---
+++ app/src/main/java/com/schapp/akbar/logccscan/waveorderlist.java
@@ -0,0 +1,16 @@
+package com.schapp.akbar.logccscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class waveorderlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waveorderlist);
+    }
+
+
+
+}
